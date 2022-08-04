e55319fc309064995bbef93e9c225c9d3df97cf2
yemrearslan <yemrearslan@hotmail.com>
2022-08-04 23:10:49 +0300

    nvim-tree update fix

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -26,7 +26,7 @@ nvim_tree.setup {
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
+  hijack_directories = {
     enable = true,
     auto_open = true,
   },
@@ -62,7 +62,6 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
@@ -81,6 +80,7 @@ nvim_tree.setup {
    actions = {
         open_file = {
             quit_on_open = false,
+            resize_window = true,
             window_picker = {
                 enable = false,
             }
