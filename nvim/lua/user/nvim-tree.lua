f8f0f66cb39f25df0fb741712f2e316b705e68dd
Emre Arslan <yemrearslan@hotmail.com>
2023-11-14 17:54:00 +0300

    wip

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -9,12 +9,7 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup({
   on_attach = on_attach,
@@ -63,7 +58,7 @@ nvim_tree.setup({
   view = {
     width = 30,
     --height = 30,
-    hide_root_folder = false,
+    -- hide_root_folder = false,
     side = "left",
     number = false,
     relativenumber = false,
